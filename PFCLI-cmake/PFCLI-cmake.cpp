6873d410735cabf8a4a1622a0332b72870f26ebf
Guest-11111111 <nanda.swagatika@gmail.com>
2025-11-15 16:57:57 -0600

    Add Visual Studio's CMake Template

---
+++ PFCLI-cmake/PFCLI-cmake.cpp
@@ -0,0 +1,12 @@
+﻿// PFCLI-cmake.cpp : Defines the entry point for the application.
+//
+
+#include "PFCLI-cmake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
