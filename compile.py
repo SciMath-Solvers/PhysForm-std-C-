0d8fc3a0f305e15bc3579fa42ffa839391cccb7a
Guest-11111111 <nanda.swagatika@gmail.com>
2025-11-15 16:05:56 -0600

    Update compile.py for faster and more effcient compilation via g++

--- compile.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.system('cl /EHsc /Fo"obj\\" /Fe"builds\\PhysForm.exe" src-CLI\main.cpp')
---
+++ src-CLI/compile.py
@@ -0,0 +1,4 @@
+import os
+
+os.system(f"""g++ main.cpp -o builds/PhysiumFormulae.exe""")
+print("Compilation finished. Executable created at builds/PhysiumFormulae.exe")
