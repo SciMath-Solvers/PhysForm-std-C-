8ab9abc24d80dcd72430466fa108e8239c450d65
PhysForm <nanda.swagatika@gmail.com>
2025-11-15 15:43:37 -0600

    Update main.cpp

--- src-CLI/main.cpp
+++ src-CLI/main.cpp
@@ -5,12 +5,40 @@ extern "C" {}
 
 typedef const char* str;
 typedef long double ldbl;
+using namespace std;
 
-void text(str ctx){
-    std::cout << ctx << std::endl;
-};
+void text(const char* text) {
+	cout << text << endl;
+}
 
-int main() {
-    
+dbl input() {
+    dbl I;
+    cin >> I >> "";
+    return I;
+}
+
+int main()
+{
+    text("------------------------------------------------ Physium Formulae for Std C++ (CLI) ------------------------------------------------");
+    text("                                                              v1.0.0\n");
+    text("                                                            [1] Solvers");
+    text("                                                            [2] Formulas");
+    text("                                                            [3] Credits");
+    text("                                                            [4] App Info");
+    text("                                                            [5] Exit");
+    input();
     return 0;
 }
+
+/*
+to compile:
+
+g++ main.cpp -o PhysForm
+
+or 
+
+cl hello.cpp -o PhysForm
+
+    cl /EHsc /Fo"obj\\" /Fe"builds\\PhysForm.exe" main.cpp
+
+*/
