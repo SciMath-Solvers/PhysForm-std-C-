57b2ff8eb255ac7589fea6e500b54656f74993c4
Guest-11111111 <nanda.swagatika@gmail.com>
2025-11-15 21:36:39 -0600

    Failed Compile

---
+++ PFCLI-cmake/main.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include "PFCLI-cmake.h"
+
+using namespace std;
+
+typedef const char* str;
+
+static void text(str ctx) {
+	cout << ctx << endl;
+};
+
+int main()
+{
+	text("--------------------------------Physium Formulae for Standard C++ 23 App Version: v1.0.0--------------------------------");
+	cin >> "Press Enter to continue...";
+	return 0;
+}
+// use cl PFCLI-cmake.cpp -o PhysiumFormulae.exe for compile?
